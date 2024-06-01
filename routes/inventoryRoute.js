a2bafee43e4df989a0afd2db2979e899950ad245
men21049 <men21049@byui.edu>
2024-05-31 19:34:19 -0600

    week04 route bug

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -1,16 +1,16 @@
 // Needed resources
 const express = require("express")
 const router = new express.Router()
-const invController = require("../controllers/invController")
 const utilities = require("../utilities")
+const invController = require("../controllers/invController")
 const regValidate = require('../utilities/classification-validation')
 const newInvValidate = require('../utilities/inventory-validation')
 
-router.get("/type/:classificationId", utilities.handleErrors(invController.buildByClassificationId))
-router.get("/detail/:inv_Id", utilities.handleErrors(invController.detailsByInvetoryId))
-router.get("/new-classification", utilities.handleErrors(invController.buildClassification))
-router.get("/new-inventory", utilities.handleErrors(invController.buildInventory))
-router.get("/", utilities.handleErrors(invController.invManagement))
+router.get("/type/:classificationId",utilities.handleErrors(invController.buildByClassificationId))
+router.get("/detail/:inv_Id",utilities.handleErrors(invController.detailsByInvetoryId))
+router.get("/new-classification",utilities.handleErrors(invController.buildClassification))
+router.get("/new-inventory",utilities.handleErrors(invController.buildInventory))
+router.get("/",utilities.handleErrors(invController.invManagement))
 
 router.post(
     "/add-classification",
